37914876591668f2e3aa57d9a10382af8d0efe96
Guillaume Simonet <84441663+GuillaumeSimonet17@users.noreply.github.com>
2023-01-25 16:29:33 +0100

    Add files via upload

---
+++ gnl/intra-uuid-33aada33-2c76-4b82-af78-232fe876dde3-4560582-gusimone/get_next_line_utils.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gusimone <gusimone@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/13 16:31:47 by gusimone          #+#    #+#             */
+/*   Updated: 2022/11/22 10:02:06 by gusimone         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+void	*ft_calloc( size_t elementCount, size_t elementSize)
+{
+	unsigned char		*tab;
+	size_t				i;
+
+	i = 0;
+	tab = (void *)malloc(elementCount * elementSize);
+	if (!tab)
+		return (NULL);
+	while (i < elementCount * elementSize)
+	{
+		tab[i] = 0;
+		i++;
+	}
+	return (tab);
+}
+
+size_t	ft_strlen(char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+size_t	ft_is_new_line(char *buf)
+{
+	size_t	i;
+
+	i = 0;
+	while (buf[i] && buf[i] != '\n')
+		i++;
+	return (i);
+}
